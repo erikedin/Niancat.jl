9508cf8baf22de942ffcf3ca6c51886386e6f558
Erik Edin <erikedin@users.noreply.github.com>
2021-05-29 23:40:11 +0200

    service: simplest HTTP POST implementation

--- src/Http.jl
+++ src/Http.jl
@@ -1,13 +1,18 @@
 module Http
 
+using HTTP
+
 abstract type HttpClient end
 
 post(::HttpClient, uri::String, body::String) = error("Implement post in HttpClient subtypes")
 
 struct RealHttpClient <: HttpClient end
 
-# TODO Implement this
-post(::RealHttpClient, uri::String, body::String) = nothing
+function post(::RealHttpClient, uri::String, body::String)
+    HTTP.request("POST", uri, [], body)
+
+    nothing
+end
 
 export HttpClient, post
 export RealHttpClient
