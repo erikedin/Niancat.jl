dc777dafb4e4bab625779edff30244a2a7161f24
Erik Edin <erikedin@users.noreply.github.com>
2021-05-31 21:39:38 +0200

    niancat: missing Slack formatting for NoPuzzleSet

--- src/Games/NiancatFormatting.jl
+++ src/Games/NiancatFormatting.jl
@@ -3,6 +3,7 @@ import Niancat.Formatters: format
 
 format(::SlackFormatter, r::Correct) = "Ordet $(r.word.w) är korrekt!"
 format(::SlackFormatter, r::PuzzleIs) = r.puzzle
+format(::SlackFormatter, r::NoPuzzleSet) = "Det finns inget pussel än."
 format(::SlackFormatter, r::Incorrect) = "Ordet $(r.word.w) finns inte med i SAOL."
 format(::SlackFormatter, r::Rejected) = "Inte ett giltigt pussel."
 format(::SlackFormatter, r::NewPuzzle) = "Nya pusslet är $(r.puzzle)."
